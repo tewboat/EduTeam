1789f19b0844e32fb6cb42421521b85864a09018
Ilya Zhdanov <53978939+tewboat@users.noreply.github.com>
2021-11-24 23:35:35 +0500

    deleted unused classes

--- Application Core/Domain/Project.cs
+++
@@ -1,7 +0,0 @@
-namespace ApplicationCore.Domain
-{
-    public class Project
-    {
-        
-    }
-}
--- Application Core/Domain/User.cs
+++
@@ -1,9 +0,0 @@
-namespace ApplicationCore.Domain
-{
-    public class User
-    {
-        public int Id { get; }
-        public string Nickname { get; private set; }
-        
-    }
-}
