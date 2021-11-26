6d5e6ad3fedb3becd1a244ae8e261d22fe8e788e
PavelLovygin <55824376+PavelLovygin@users.noreply.github.com>
2021-11-26 22:50:49 +0500

    registration and login bottons

--- User Interface/Controllers/HomeController.cs
+++ User Interface/Controllers/HomeController.cs
@@ -33,5 +33,15 @@ namespace User_Interface.Controllers
         {
             return View(new ErrorViewModel {RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier});
         }
+
+        public IActionResult Registration()
+        {
+            return View();
+        }
+
+        public IActionResult Login()
+        {
+            return View();
+        }
     }
 }
