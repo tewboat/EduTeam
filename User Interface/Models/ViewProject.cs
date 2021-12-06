b606ede3c820c6377f7f0403bf8b7c5e58e8a7bd
PavelLovygin <55824376+PavelLovygin@users.noreply.github.com>
2021-12-06 13:35:44 +0500

    Finish ViewUser and ViewProject

--- User Interface/Models/ViewProject.cs
+++ User Interface/Models/ViewProject.cs
@@ -1,5 +1,7 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
+using ApplicationCore.Project;
 
 namespace User_Interface.Models
 {
@@ -7,8 +9,23 @@ namespace User_Interface.Models
     {
         public string Name { get; set; }
         public string Description { get; set; }
+        public DateOnly DateCreation { get; set; }
+        public bool IsScrumUsed { get; set; }
+        public Language Language { get; set; }
+        public bool IsPersonalMeetingsPreferred { get; set; }
         public List<ViewUser> Members { get; set; }
         public List<ViewUser> Invitations { get; set; }
         public List<ViewUser> Requests { get; set; }
+
+        public ViewProject(Project project)
+        {
+            Name = project.Name;
+            Description = project.Description;
+            DateCreation = project.DateCreation;
+            IsScrumUsed = project.IsScrumUsed;
+            Language = project.Language;
+            IsPersonalMeetingsPreferred = project.IsPersonalMeetingsPreferred;
+            var a = project.Members.Select(member => new ViewUser(member.User));
+        }
     }
 }
--- User Interface/Models/ViewUser.cs
+++ User Interface/Models/ViewUser.cs
@@ -1,17 +1,29 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using ApplicationCore;
+using ApplicationCore.User;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using User_Interface.Views.Home;
 
 namespace User_Interface.Models
 {
-    public class ViewUser : PageModel
+    public class ViewUser
     {
         public string FirstName { get; set; }
         public string SecondName { get; set; }
         public string Nickname { get; set; }
         public string Email { get; set; }
+        public string Discription { get; set; }
         public List<ViewProject> Projects { get; set; }
+
+        public ViewUser(User user)
+        {
+            FirstName = user.FirstName;
+            SecondName = user.SecondName;
+            Nickname = user.Nickname;
+            Email = user.Email;
+            Discription = user.Description;
+        }
     }
 }
