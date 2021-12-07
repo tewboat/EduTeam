3fb4e4be447f1311e704b30aaa268dc95aa27cee
PavelLovygin <55824376+PavelLovygin@users.noreply.github.com>
2021-12-07 20:26:29 +0500

    add projects page

--- User Interface/Controllers/ProjectsController.cs
+++ User Interface/Controllers/ProjectsController.cs
@@ -15,30 +15,137 @@ namespace User_Interface.Controllers
     {
         private readonly ILogger<HomeController> _logger;
         private ApplicationContext context;
+        private List<Project> _projects;
+        private int productPage;
+        private Func<Project, bool> filter;
+        private Func<Project, object> order;
 
         public ProjectsController(ILogger<HomeController> logger, ApplicationContext applicationContext)
         {
             _logger = logger;
             context = applicationContext;
+            _projects = new List<Project>() 
+                { new Project(
+                    "EduTeam", 
+                    "Сервис для поиска команды для учебных проектов", 
+                    Language.Rus, 
+                    true, 
+                    true)
+                    { RequiredTeamRoles = new List<TeamRole>()
+                    {
+                        new TeamRole(){Name = "Дизайнер", Description = "Дизайн веб-сервиса, макет в фигме"},
+                        new TeamRole(){Name = "Верстальщик сайта", Description = "Вёрстка сайта в соответствии с макетом"},
+                        new TeamRole(){Name = "Бэкендер C#", Description = "Работа с базами данных"}
+                    }},
+                    new Project(
+                        "EduTeam_2", 
+                        "Сервис для поиска команды для учебных проектов", 
+                        Language.Rus, 
+                        true, 
+                        true)
+                    { RequiredTeamRoles = new List<TeamRole>()
+                    {
+                        new TeamRole(){Name = "Дизайнер", Description = "Дизайн веб-сервиса, макет в фигме"},
+                        new TeamRole(){Name = "Верстальщик сайта", Description = "Вёрстка сайта в соответствии с макетом"},
+                        new TeamRole(){Name = "Бэкендер C#", Description = "Работа с базами данных"}
+                    }},
+                    new Project(
+                        "EduTeam_2", 
+                        "Сервис для поиска команды для учебных проектов", 
+                        Language.Rus, 
+                        true, 
+                        true)
+                    { RequiredTeamRoles = new List<TeamRole>()
+                    {
+                        new TeamRole(){Name = "Дизайнер", Description = "Дизайн веб-сервиса, макет в фигме"},
+                        new TeamRole(){Name = "Верстальщик сайта", Description = "Вёрстка сайта в соответствии с макетом"},
+                        new TeamRole(){Name = "Бэкендер C#", Description = "Работа с базами данных"}
+                    }},
+                    new Project(
+                        "EduTeam_2", 
+                        "Сервис для поиска команды для учебных проектов", 
+                        Language.Rus, 
+                        true, 
+                        true)
+                    { RequiredTeamRoles = new List<TeamRole>()
+                    {
+                        new TeamRole(){Name = "Дизайнер", Description = "Дизайн веб-сервиса, макет в фигме"},
+                        new TeamRole(){Name = "Верстальщик сайта", Description = "Вёрстка сайта в соответствии с макетом"},
+                        new TeamRole(){Name = "Бэкендер C#", Description = "Работа с базами данных"}
+                    }},
+                    new Project(
+                        "EduTeam_2", 
+                        "Сервис для поиска команды для учебных проектов", 
+                        Language.Rus, 
+                        true, 
+                        true)
+                    { RequiredTeamRoles = new List<TeamRole>()
+                    {
+                        new TeamRole(){Name = "Дизайнер", Description = "Дизайн веб-сервиса, макет в фигме"},
+                        new TeamRole(){Name = "Верстальщик сайта", Description = "Вёрстка сайта в соответствии с макетом"},
+                        new TeamRole(){Name = "Бэкендер C#", Description = "Работа с базами данных"}
+                    }},
+                    new Project(
+                        "EduTeam_2", 
+                        "Сервис для поиска команды для учебных проектов", 
+                        Language.Rus, 
+                        true, 
+                        true)
+                    { RequiredTeamRoles = new List<TeamRole>()
+                    {
+                        new TeamRole(){Name = "Дизайнер", Description = "Дизайн веб-сервиса, макет в фигме"},
+                        new TeamRole(){Name = "Верстальщик сайта", Description = "Вёрстка сайта в соответствии с макетом"},
+                        new TeamRole(){Name = "Бэкендер C#", Description = "Работа с базами данных"}
+                    }}
+                };
+            productPage = 1;
+            filter = project => true;
+            order = project => project.DateCreation;
         }
         public int PageSize = 4;
         
         
         public ViewResult Projects(
-            int productPage = 1,
-            Func<Project, bool> filter = null, 
-            Func<Project, object> order = null)
+            int productPage = 1)
         {
-            filter ??= _ => true;
-            order ??= project => project.DateCreation;
             return View(
-                context.Projects
+                _projects
                     .Where(filter)
                     .OrderByDescending(order)
                     .Skip((productPage - 1) * PageSize)
-                    .Select(project => new ViewProject(project))
+                    .Select(project =>
+                        new ViewProject()
+                        {
+                            Name = project.Name,
+                            Description = project.Description,
+                            DateCreation = project.DateCreation.ToString(),
+                            IsScrumUsed = project.IsScrumUsed,
+                            Language = project.Language.ToString(),
+                            RequiredTeamRoles = project.RequiredTeamRoles
+                                .Select(role => new ViewTeamRole()
+                                {
+                                    Name = role.Name,
+                                    Description = role.Description
+                                })
+                                .ToList(),
+                            IsPersonalMeetingsPreferred = project.IsPersonalMeetingsPreferred,
+                            Members = project.Members.Select(member =>
+                                new ViewUser()
+                                {
+                                    FirstName = member.User.FirstName,
+                                    SecondName = member.User.SecondName,
+                                    Nickname = member.User.Nickname,
+                                    Email = member.User.Email,
+                                    Discription = member.User.Description
+                                }).ToList()})
                     .ToList()
                 );
+        }
+
+        [HttpPost]
+        public ViewResult Project(ViewProject project)
+        {
+            return View(project);
         }
     }
 }
