2410ed8c3d95aaf9fbf1ef781a243c136b7d9861
PavelLovygin <55824376+PavelLovygin@users.noreply.github.com>
2021-12-18 21:07:00 +0500

    add AddTeamRole

--- User Interface/Controllers/ProfileController.cs
+++ User Interface/Controllers/ProfileController.cs
@@ -1,12 +1,22 @@
 using System;
+using Application;
 using ApplicationCore.User;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 using User_Interface.Models;
 
 namespace User_Interface.Controllers
 {
     public class ProfileController : Controller
     {
+        private readonly ILogger<HomeController> _logger;
+        private ApplicationContext context;
+
+        public ProfileController(ILogger<HomeController> logger, ApplicationContext applicationContext)
+        {
+            _logger = logger;
+            context = applicationContext;
+        }
         
         public ViewResult EditUserProfile(Guid guid)
         {
@@ -17,6 +27,20 @@ namespace User_Interface.Controllers
                     return View(UsersController.ConvertToView(user));
             }
             throw new ArgumentException("No project with this Guid");
+        }
+
+        [HttpGet]
+        public ActionResult AddTeamRole()
+        {
+            return PartialView();
+        }
+
+        [HttpPost]
+        public ActionResult AddTeamRole(ViewTeamRole teamRole)
+        {
+            // TODO: Добавить роль пользователя
+            
+            return RedirectToAction("EditUserProfile");
         }
     }
 }
